c7a30b14ca8ad7638dbab0b77952d4393bd67e79
Ridhishaa Ardham <ridhiardham@gmail.com>
2025-09-17 21:05:22 +0530

    Create armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int a,b,n;int x=0;
+    printf("Enter the number and the no of digits\n");
+    scanf("%d %d",&a,&n);
+    int c=a;
+    for(int i=0;i<n;i++)
+    { b=a%10;
+    a=a/10;
+    x=x+(b*b*b);
+    }
+    if (c==x)
+    printf("It is an armstrong no");
+    else
+    printf("it is not");
+    return 0;
+}
