cafb686b87112819cc556c64a0e307228f5cdc79
Ridhishaa Ardham <ridhiardham@gmail.com>
2025-09-20 20:43:25 +0530

    Update maxconsecutivesones.java

--- maxconsecutivesones.java
+++ maxconsecutivesones.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int i,temp=0,temp1=0; 
+       int l= nums.length;
+        for(i=0;i<l;i++)
+        {
+            if(nums[i]==1)
+            {temp++;
+                temp1=Math.max(temp,temp1);
+            }
+            else
+            temp=0;
+        }
+        return temp1;
+            }
+}
