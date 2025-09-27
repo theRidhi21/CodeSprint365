0d3b086ac59b4bc5ab19278caf7eeae271a866ac
Ridhishaa Ardham <ridhiardham@gmail.com>
2025-09-27 18:56:00 +0530

    Create binary to decimal.c

---
+++ binary to decimal.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+void main(){
+    int a,b=0;
+    scanf("%d",&a);
+    for(int i=0;a>0;i++){
+        int x = a%10;
+        a=a/10;
+        int y= pow(2,i);
+        b=b+(x*y);
+    }
+    printf("%d \n",b);
+}
