5671f5d3da1655091d118552d241222642024707
Ridhishaa Ardham <ridhiardham@gmail.com>
2025-10-07 20:20:04 +0530

    Update concatenationarray.java

--- concatenationarray.java
+++ concatenationarray.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] getConcatenation(int[] nums) {
-         int n=nums.length;
+        int n=nums.length;
         int[] ans = new int[2*n];
         for(int i=0;i<n;i++)
         {
