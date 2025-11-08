9959e8f365aad217c51f759d6f9891de3756335a
Ridhishaa Ardham <ridhiardham@gmail.com>
2025-11-08 22:25:23 +0530

    Update concatenationarray.java

--- concatenationarray.java
+++ concatenationarray.java
@@ -4,7 +4,7 @@ class Solution {
         int[] ans = new int[2*n];
         for(int i=0;i<n;i++)
         {
-            ans[i]=nums[i];ugkug gg 
+            ans[i]=nums[i];
             ans[i+n]=nums[i];
         } 
         return ans;
