571e91c2402d2cec2a4ef565d33d8f97264d32b2
Ridhishaa Ardham <ridhiardham@gmail.com>
2026-02-04 18:35:57 +0530

    Update lengthoflastword.java

--- lengthoflastword.java
+++ lengthoflastword.java
@@ -4,7 +4,7 @@ class Solution {
         int i,n;
         for(i=0;i<l;i++)
         {
-            for(n=l/2;n>=0;n--)
+            for(n=l/2;n>=0;n--) 
             { 
                 arr[i]=Math.max(arr[i],arr[i+n]); 
             } 
