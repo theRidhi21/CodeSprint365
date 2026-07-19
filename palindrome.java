ae93ada4a70b879fa2eb70749053e6be23105a08
Ridhishaa Ardham <ridhiardham@gmail.com>
2026-07-19 16:16:15 +0530

    Update palindrome.java

--- palindrome.java
+++ palindrome.java
@@ -9,7 +9,24 @@ class Solution {
        for(i=c-1;t>0;i--){
         int b=t%10;
         t=t/10;
-        rev+=(int)(b*Math.pow(10,i));
+        rev+=(int)(b*Math.pow(10,i));//type casting as pow gives float
+       }
+       if(x==rev){
+        return true;
+       }
+        else {
+            return false;
+       } 
+    }
+}
+## or ##
+    class Solution {
+    public boolean isPalindrome(int x) {
+       int i,rev=0,t=x;
+       while(t>0){
+        int b=t%10;
+        t=t/10;
+        rev=rev*10+b;
        }
        if(x==rev){
         return true;
