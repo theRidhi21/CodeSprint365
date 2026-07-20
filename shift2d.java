6ec5c2b41657e80a101c927789f840729bce8607
Ridhishaa Ardham <ridhiardham@gmail.com>
2026-07-20 21:26:52 +0530

    Update shift2d.java

--- shift2d.java
+++ shift2d.java
@@ -3,7 +3,7 @@ class Solution {
         int i,j;
         int m=grid.length;
         int n=grid[0].length;int t=m*n;
-        k%=t;
+        k%=t; 
         List<List<Integer>> ans = new ArrayList<>();
         for (i = 0; i < m; i++) {
             List<Integer> row = new ArrayList<>();
