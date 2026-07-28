aab491507ebd370f7c8491e9e41ed0570ecfcaec
Ridhishaa Ardham <ridhiardham@gmail.com>
2026-07-28 08:50:40 +0530

    Create movezeroes.java

---
+++ movezeroes.java
@@ -0,0 +1,13 @@
+lass Solution {
+    public void moveZeroes(int[] nums) {
+        int l=nums.length;
+        for(int i=0;i<l;i++){
+            for(int j=0;j<l-1;j++){
+                if(nums[j]==0){
+                    nums[j]=nums[j+1];
+                    nums[j+1]=0;
+                }
+            }
+        }
+    }
+}
