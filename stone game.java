3acf64341f77091030ee95ce63545c6d28954155
Ridhishaa Ardham <ridhiardham@gmail.com>
2026-08-02 22:14:43 +0530

    Create stone game.java

---
+++ stone game.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        return true;
+    }
+}
+
+This question actually amazed me!!!!!!!!!!!!!!!!!!!!!!!!
+Alice and Bob , Alice always positions herself to win the game by making sure she gets the most number of piles.
+As it contains even no of stones , it can't tie.
