25ca3bc7a7094ebec9b7c561e59b02af7dbaf681
Ridhishaa Ardham <ridhiardham@gmail.com>
2026-09-08 21:42:04 +0530

    Create uniform parity array II.java

---
+++ uniform parity array II.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        int minOdd = Integer.MAX_VALUE;
+        for (int x : nums1) {
+            if (x % 2 != 0) {
+                minOdd = Math.min(minOdd, x);
+            }
+        }
+        if (minOdd == Integer.MAX_VALUE) {
+            return true;
+        }
+        for (int x : nums1) {
+            if (x % 2 == 0 && x <= minOdd) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
